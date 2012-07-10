abf3d6cf9097a2cd73ea9e0f00e1805be4462def
Corey Fisher <crfish@mac.com>
2012-07-10 15:51:14 -0400

    good nab update

--- ci_day6/application/views/logged_in_area.php
+++ ci_day6/application/views/logged_in_area.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 
 <html lang="en">
-<!--  <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
 	<title>untitled</title>
-</head>-->
+</head>
 <body>
 	<h2>Welcome Back, <?php echo $this->session->userdata('username'); ?>!</h2>
      <p>This section represents the area that only logged in members can access.</p>
