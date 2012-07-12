cccae33692d25dbe7f1d5ecf4cca0cc44708e6fe
Tommy <rthomasmaxwell@yahoo.com>
2012-07-12 14:49:16 -0400

    CMS/Prototype Integration

---
+++ Prototype:CMS Integration/magicstuff/admin/application/controllers/nav.php
@@ -0,0 +1,18 @@
+<?php
+
+class Nav extends Controller 
+{
+	function __construct()
+	{
+		parent::Controller();
+/* 		$this->is_logged_in(); */
+	}
+	
+	function index()
+	{
+		$this->load->view('includes/header');
+		$this->load->view('cms/nav');
+		$this->load->view('nav');
+	}
+}
+?>
