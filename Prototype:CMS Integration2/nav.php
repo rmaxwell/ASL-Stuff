871f923b88e91bce1b4247259a48c004082ea664
Corey Fisher <crfish@mac.com>
2012-07-13 15:34:59 -0400

    nav file

---
+++ Prototype:CMS Integration2/nav.php
@@ -0,0 +1,13 @@
+<div id="navwrap">
+<a href="/magicstuff/admin/index.php/site/members_area"><img id="logo" src="../img/logo.png"></a>
+	<nav>
+		<ul>
+			<li><?=anchor('gallery', 'Gallery');?></li>
+			<li><?=anchor('videos', 'Videos');?></li>
+			<li><?=anchor('bio', 'Bio');?></li>
+			<li><?=anchor('blog', 'Blog');?></li>
+			<li><?=anchor('nav', 'Nav');?></li>
+			<li><?=anchor('booking', 'Booking');?></li>
+		</ul>
+	</nav>
+</div>
