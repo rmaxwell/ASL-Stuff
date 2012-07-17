23154d4fefece0a6f70c352835c611c0436baed9
Tommy <rthomasmaxwell@yahoo.com>
2012-07-17 09:38:31 -0400

    css and CMS h and F

--- Prototype:CMS Integration/magicstuff/admin/application/views/includes/header.php
+++ Prototype:CMS Integration/magicstuff/admin/application/views/includes/header.php
@@ -3,38 +3,9 @@
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
-	<title>Sign Up!</title>
-	<link rel="stylesheet" href="<?php echo base_url();?>css/cmsstyle.css" type="text/css" media="screen" />
+	<title><?=$title?></title>
+	<link rel="stylesheet" href="<?php echo base_url();?>/css/style.css" type="text/css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/css/imgareaselect-default.css" />
 	
 </head>
 <body>
-	<script type="text/javascript" src="../jscripts/tiny_mce/tiny_mce.js"></script>
-
-		<script type="text/javascript">
-			tinyMCE.init({
-				mode : "textareas",
-				theme : "advanced",
-				plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,iespell,inlinepopups,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave,visualblocks",
-		
-				// Theme options
-				theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
-				theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,code,|,visualchars,forecolor,backcolor",
-				theme_advanced_toolbar_location : "top",
-				theme_advanced_toolbar_align : "left",
-				theme_advanced_statusbar_location : "bottom",
-				theme_advanced_resizing : true,
-		
-				// Style formats
-				style_formats : [
-					{title : 'Bold text', inline : 'b'},
-					{title : 'Red text', inline : 'span', styles : {color : '#ff0000'}},
-					{title : 'Red header', block : 'h1', styles : {color : '#ff0000'}},
-					{title : 'Example 1', inline : 'span', classes : 'example1'},
-					{title : 'Example 2', inline : 'span', classes : 'example2'},
-					{title : 'Table styles'},
-					{title : 'Table row 1', selector : 'tr', classes : 'tablerow1'}
-				],
-			});
-		</script>
-</body>
-</html>
