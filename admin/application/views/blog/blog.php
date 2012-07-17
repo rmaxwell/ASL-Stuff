f07606c34941fd437c747fd74a81eb2efbee145d
KidderDog <kidderdog@earthlink.net>
2012-07-17 09:28:27 -0400

    Moved SQL and removed two folder
    Admin and ci_day6

--- admin/application/views/blog/blog.php
+++
@@ -1,7 +0,0 @@
-<?php
-	foreach($blogs as $blog){
-		echo '<h2>'.anchor('blog/editBlog/'.$blog['blog_id'], $blog['blog_title']).'<button>On or Off</button></h2>';
-	}	
-?>
-
-<div><a href="">Add New Blog</a></div>
--- ci_day6/application/views/cms/nav.php
+++
@@ -1,8 +0,0 @@
-<h1 class="logo"><a href="/admin/index.php/site/members_area">x</a></h1>
-<nav>
-	<?=anchor('gallery', 'Gallery');?>
-	<?=anchor('videos', 'Videos');?>
-	<?=anchor('bio', 'Bio');?>
-	<?=anchor('blog', 'Blog');?>
-	<?=anchor('nav', 'Nav');?>
-</nav>
