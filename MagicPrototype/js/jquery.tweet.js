995a4fb2b4a261553ea96dcab30b7c94b769a6f8
Brad Bohen <bbohen@fullsail.edu>
2012-07-21 10:43:57 -0400

    Updating Stuff

--- MagicPrototype/js/jquery.tweet.js
+++ MagicPrototype/js/jquery.tweet.js
@@ -19,12 +19,14 @@
       retweets: true,                           // [boolean]  whether to fetch (official) retweets (not supported in all display modes)
       intro_text: null,                         // [string]   do you want text BEFORE your your tweets?
       outro_text: null,                         // [string]   do you want text AFTER your tweets?
-      join_text:  null,                         // [string]   optional text in between date and tweet, try setting to "auto"
+      join_text:  null,   
+      /*                      // [string]   optional text in between date and tweet, try setting to "auto"
       auto_join_text_default: "I said,",        // [string]   auto text for non verb: "I said" bullocks
       auto_join_text_ed: "I",                   // [string]   auto text for past tense: "I" surfed
       auto_join_text_ing: "I am",               // [string]   auto tense for present tense: "I was" surfing
       auto_join_text_reply: "I replied to",     // [string]   auto tense for replies: "I replied to" @someone "with"
       auto_join_text_url: "I was looking at",   // [string]   auto tense for urls: "I was looking at" http:...
+      */
       loading_text: null,                       // [string]   optional loading text, displayed while tweets load
       refresh_interval: null,                   // [integer]  optional number of seconds after which to reload tweets
       twitter_url: "twitter.com",               // [string]   custom twitter url, if any (apigee, etc.)
